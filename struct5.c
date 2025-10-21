830b568ccfc0a9758ae6d040dbb54ebe66121481
murilomendes2180 <murilomendes20172017@outlook.com>
2025-10-21 19:10:06 -0300

    Update struct5.c

--- struct5.c
+++ struct5.c
@@ -17,13 +17,15 @@ void carregar_registro(struct abelha  reg_abelha [TF]){
 	for (i=0; i<TF;i++){
 	fflush(stdin);
 	printf("\nregistro[%d]:", i + 1);
+	printf("informe o estado:");
 	gets(reg_abelha[i].estado);
+	printf("informe a regiao:");
 	gets(reg_abelha[i].regiao);
 	printf("informe o codigo:");
 	scanf("%d", &reg_abelha[i].codigo);
 	
 	do{
-		printf("informe a qualificação (1-forte 2-medio 3 - fraco)");
+		printf("informe a qualificação (1-forte 2-medio 3-fraco)");
 		scanf("%d",&reg_abelha[i].qualificacao);
 	}while(reg_abelha[i].qualificacao != 1 && reg_abelha[i].qualificacao != 2  && reg_abelha[i].qualificacao != 3);
 	}
@@ -34,10 +36,9 @@ void exibir(struct abelha reg_abelha[TF]){
 	printf("<<exibir vetor registro>>\n\n");
 		for(i=0;i<TF;i++){
 			printf("\nregistro: %d", i + 1);
-			printf("\nidade: %d", reg_abelha[i].codigo);
-			printf("\nnumero de filho: %s", reg_abelha[i].estado);
-			printf("\nsalario: %s", reg_abelha[i].regiao);
-		
+			printf("\ncodigo: %d", reg_abelha[i].codigo);
+			printf("\nestado: %s", reg_abelha[i].estado);
+			printf("\nregiao: %s", reg_abelha[i].regiao);
 	}
 }
 
@@ -46,8 +47,5 @@ void main(){
 	struct abelha reg_abelha[TF];
 	carregar_registro(reg_abelha);
 	exibir(reg_abelha);
-	media(reg_abelha);
-
-
 
 }
